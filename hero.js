2a7c037c79c7ef850a3060817ba67efc094f69fd
DennieBocon <bocondenniesburt@gmail.com>
2025-09-15 08:57:20 +0800

    Added Mage

--- hero.js
+++ hero.js
@@ -19,5 +19,14 @@ class Warrior extends Hero(){
     }
 }
 
+class Mage extends Hero{
+    constructor(name, health, attack, mana){
+        super(name, health, attack);
+        this.mana = mana;
+    }
+}    
 const warrior = new Warrior("Thorin",100,10);
 Warrior.useAbility()
+
+const mage = new Mage("Gandalf",80,8,50);
+mage.useAbility();
